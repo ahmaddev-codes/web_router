e249b6fb54fac447c5b5745fe2a059ec63ed2cea
Tijani Ahmad <omegabliss23@gmail.com>
2024-04-03 23:32:13 +0100

    Code: updated frontend code

--- app/index.js
+++ app/index.js
@@ -4,21 +4,24 @@ function submitForm() {
     const search = document.getElementById('search').value;
     // send data to the backend
     // change the URL to the backend URL
-    fetch('/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            search: search
-        })
-    }).then(response => response.json())
-        .then(data => {
-            // handle response from the backend
-            console.log(data);
-            document.getElementById('search').value = data;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    try {
+        fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                search: search
+            })
+        }).then(response => response.json())
+            .then(data => {
+                // handle response from the backend
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
